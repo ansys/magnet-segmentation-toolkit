ef0280e6e4ba05688d70b26c81094a2cd3d636b1
Giulia Malinverno <giulia.malinverno@ansys.com>
2023-03-09 16:35:10 +0100

    fix and update toml + clean

---
+++ src/ansys/aedt/toolkits/motor/__init__.py
@@ -0,0 +1,11 @@
+"""
+pyaedt-toolkits.
+ansys.pymotorcad_pyaedt_toolkit
+"""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = "0.1.dev0"
