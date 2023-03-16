47f5b2fb8fa8f38d38ba93d5721a396dd9df38c2
Giulia Malinverno <giulia.malinverno@ansys.com>
2023-03-16 16:18:12 +0100

    add doc and add pydocstyle in yaml file

--- src/ansys/aedt/toolkits/motor/aedt_export.py
+++ src/ansys/aedt/toolkits/motor/aedt_export.py
@@ -1,12 +1,37 @@
 import os
 
-
-from pyaedt import Desktop
-from pyaedt import Maxwell3d
+from pyaedt import Desktop, Maxwell3d
 
 
 class AEDTexport:
+    """Provides an AEDT instance.
+
+     Create a Maxwell3D instance and the motor 3D model by running .vbs script.
+
+    Parameters
+    ----------
+    vbs_file_path : str
+        File path of the vbs script.
+    working_dir : str
+        Working directory where to save th project to.
+
+    Examples
+    --------
+    >>>> aedt = AEDTexport()
+    Set the geometry model such as the axial length,
+    apply boundary conditions and
+    remove from model unclassified objects
+    >>>> aedt.set_model()
+    Set mesh for rotor, stator, windings and magnets
+    >>>> aedt.mesh_settings()
+    Analyze model
+    >>>> aedt.analyze_model()
+    Save and close project
+    >>>> aedt.save_and_close()
+    """
+
     def __init__(self, vbs_file_path, working_dir):
+        """Init."""
         self._vbs_file_path = vbs_file_path
         self._desktop = Desktop(specified_version="2023.1")
         self._desktop.odesktop.RunScript(self._vbs_file_path)
@@ -14,23 +39,47 @@ class AEDTexport:
         self.working_dir = working_dir
 
     def set_model(self):
+        """Set geometry model.
+
+        Set axial length, boundary conditions and remove from model unclassified objects.
+        """
         self.maxwell["HalfAxial"] = 1
+        if self.maxwell["HalfAxial"] == 1:
+            self._apply_boundary_conditions()
         for obj in self.maxwell.modeler.unclassified_objects:
             obj.model = False
 
     def mesh_settings(self):
-        # apply mesh sizing
+        """Apply mesh settings.
+
+        Apply mesh to rotor, stator, windings and magnets.
+        """
         # windings
         copper_objs_list = self.maxwell.modeler.get_objects_by_material("Copper (Pure)_50C")
-        self.maxwell.mesh.assign_length_mesh(copper_objs_list, maxlength=8, meshop_name="copper_objs_mesh")
+        self.maxwell.mesh.assign_length_mesh(
+            copper_objs_list, maxlength=8, meshop_name="copper_objs_mesh"
+        )
         # rotor - stator
         rotor_stator = self.maxwell.modeler.get_objects_by_material("M250-35A_20C")
-        self.maxwell.mesh.assign_length_mesh(rotor_stator, maxlength=8, meshop_name="rotor_stator_mesh")
+        self.maxwell.mesh.assign_length_mesh(
+            rotor_stator, maxlength=8, meshop_name="rotor_stator_mesh"
+        )
         # magnets
         magnets = self.maxwell.modeler.get_objects_by_material("N30UH_20C")
         self.maxwell.mesh.assign_length_mesh(magnets, maxlength=2, meshop_name="magnets_mesh")
 
-    def apply_boundary_conditions(self):
+    def analyze_model(self):
+        """Analyze model."""
+        self.maxwell.analyze_setup(self.maxwell.setups[0])
+
+    def save_and_close(self):
+        """Save and close."""
+        self.maxwell.save_project(
+            os.path.join(self.working_dir, "{}.aedt".format(self.maxwell.project_name))
+        )
+        self._desktop.release_desktop(False, False)
+
+    def _apply_boundary_conditions(self):
         # apply insulating BC on all magnet faces
         magnets = self.maxwell.modeler.get_objects_by_material("N30UH_20C")
         self.maxwell.assign_insulating(magnets, "magnets_insulation")
@@ -41,10 +90,3 @@ class AEDTexport:
                 face_ids.append(self.maxwell.modeler[obj].bottom_face_z.id)
         self.maxwell.assign_insulating(face_ids)
         # Set Eddy Effects on Magnets (already done by export)
-
-    def analyze_model(self):
-        self.maxwell.analyze_setup(self.maxwell.setups[0])
-
-    def save_and_close(self):
-        self.maxwell.save_project(os.path.join(self.working_dir, "{}.aedt".format(self.maxwell.project_name)))
-        self._desktop.release_desktop(False, False)
--- src/ansys/aedt/toolkits/motor/motorcad_settings.py
+++ src/ansys/aedt/toolkits/motor/motorcad_settings.py
@@ -1,11 +1,38 @@
-import ansys.motorcad.core as pymotorcad
 import os
 
+import ansys.motorcad.core as pymotorcad
 from pyaedt import generate_unique_folder_name
 
 
 class MotorCADSettings:
+    """Creates a MotorCAD instance and load a predefined template.
+
+    Provides a Motor-CAD instance to set geometry,
+    LAB and electromagnetic settings in order to export the model.
+
+    Parameters
+    ----------
+    working_dir : str, optional
+        Working directory to store results and .mot file.
+        If nothing is provided a new temp folder is created.
+        Default value is ``None``.
+
+    Examples
+    --------
+    >>>> mcad = MotorCADSettings()
+    Set the geometry model
+    >>>> mcad.set_geometry_model()
+    Set LAB module, build LAB model, calculate Emag performance,
+    Set continuous performance operating points
+    >>>> mcad.set_lab_model()
+    Set and run emag calculation
+    >>>> mcad.emag_calculation()
+    Set export settings to get .vbs script
+    >>>> mcad.export_settings()
+    """
+
     def __init__(self, working_dir=None):
+        """Init."""
         self.mcad = pymotorcad.MotorCAD()
         self.mcad.set_variable("MessageDisplayState", 2)
         if working_dir:
@@ -17,6 +44,7 @@ class MotorCADSettings:
         self.vbs_file_path = os.path.join(self.working_dir, "{}.vbs".format(self.mcad_name))
 
     def set_geometry_model(self):
+        """Set geometry model."""
         self.mcad.load_template(self.mcad_name)
         self.mcad.show_magnetic_context()
         self.mcad.display_screen("Scripting")
@@ -27,56 +55,53 @@ class MotorCADSettings:
         self.mcad.save_to_file(self.mcad_file_path)
 
     def set_lab_model(self):
+        """Set lab model."""
         # LAB Module
         self.mcad.set_motorlab_context()
 
-        self.mcad.set_variable("ModelType_MotorLAB", 2)  # Model type: Saturation Model (Full Cycle)
-        self.mcad.set_variable("SatModelPoints_MotorLAB", 1)  # Model Resolution : Fine (30 Points)
-        self.mcad.set_variable("LossModel_Lab", 1)  # Loss Model : FEA Map
-        self.mcad.set_variable("ACLossMethod_Lab", 0)  # AC Loss Model : Hybrid Method
-        self.mcad.set_variable("ModelBuildSpeed_MotorLAB", 10000)  # Max speed
-        self.mcad.set_variable("MaxModelCurrent_MotorLAB", 480)  # Max current
-        self.mcad.set_variable("BuildSatModel_MotorLAB", True)  # Enable Saturation model
-        self.mcad.set_variable("BuildLossModel_MotorLAB", True)  # Enable Loss model
+        self.mcad.set_variable("ModelType_MotorLAB", 2)
+        self.mcad.set_variable("SatModelPoints_MotorLAB", 1)
+        self.mcad.set_variable("LossModel_Lab", 1)
+        self.mcad.set_variable("ACLossMethod_Lab", 0)
+        self.mcad.set_variable("ModelBuildSpeed_MotorLAB", 10000)
+        self.mcad.set_variable("MaxModelCurrent_MotorLAB", 480)
+        self.mcad.set_variable("BuildSatModel_MotorLAB", True)
+        self.mcad.set_variable("BuildLossModel_MotorLAB", True)
 
         # Build the model.
         self.mcad.clear_model_build_lab()
         self.mcad.build_model_lab()
 
-        #self.mcad.load_template("Test_e9_built")
+        # self.mcad.load_template("Test_e9_built")
 
-        # Peak performance  Torque-Speed curve
-        self.mcad.set_variable("EmagneticCalcType_Lab", 0)  # Calc type: Max Torque speed curve
-        self.mcad.set_variable("SpeedMax_MotorLAB", 10000)  # Max speed for TS curve
-        self.mcad.set_variable("SpeedMin_MotorLAB", 0)  # Min speed for TS curve
-        self.mcad.set_variable("Speedinc_MotorLAB", 500)  # Step size
-        self.mcad.set_variable("OperatingMode_Lab", 0)  # Motor mode only
-        self.mcad.calculate_magnetic_lab()  # calculate Emagnetic Performance
+        # Peak performance Torque-Speed curve
+        self.mcad.set_variable("EmagneticCalcType_Lab", 0)
+        self.mcad.set_variable("SpeedMax_MotorLAB", 10000)
+        self.mcad.set_variable("SpeedMin_MotorLAB", 0)
+        self.mcad.set_variable("Speedinc_MotorLAB", 500)
+        self.mcad.set_variable("OperatingMode_Lab", 0)
+        self.mcad.calculate_magnetic_lab()
 
         # Continuous performance operating points
-        self.mcad.set_variable("LabMagneticCoupling", 1)  # Send operating point to emag
-        self.mcad.set_variable("OpPointSpec_MotorLAB", 2)  # Max temp definition for operating point
-        self.mcad.set_variable(
-            "ThermCalcType_MotorLAB", 0
-        )  # Run steady state thermal calc to save computation time
-        self.mcad.set_variable(
-            "ThermalMapType_Lab", 0
-        )  # Thermal envelope option (relevant for max curve value option only)
-        self.mcad.set_variable("MaxWindTemp_MotorLAB", 140)  # Set winding temperature
-        self.mcad.set_variable("StatorTempDemand_Lab", 140)  # Set winding temperature
-        self.mcad.set_variable("MaxMagnet_MotorLAB", 160)  # Set magnet temperature
-        self.mcad.set_variable("RotorTempDemand_Lab", 160)  # Set magnet temperature
+        self.mcad.set_variable("LabMagneticCoupling", 1)
+        self.mcad.set_variable("OpPointSpec_MotorLAB", 2)
+        self.mcad.set_variable("ThermCalcType_MotorLAB", 0)
+        self.mcad.set_variable("ThermalMapType_Lab", 0)
+        self.mcad.set_variable("MaxWindTemp_MotorLAB", 140)
+        self.mcad.set_variable("StatorTempDemand_Lab", 140)
+        self.mcad.set_variable("MaxMagnet_MotorLAB", 160)
+        self.mcad.set_variable("RotorTempDemand_Lab", 160)
         self.mcad.set_variable("SpeedDemand_MotorLAB", 4500)
-        self.mcad.calculate_operating_point_lab()  # calculate operating point
+        self.mcad.calculate_operating_point_lab()
         shaft_power = self.mcad.get_variable("LabOpPoint_ShaftPower")
         efficiency = self.mcad.get_variable("LabOpPoint_Efficiency")
         print("Shaft Power", round(shaft_power / 1000, 2), "kW")
         print("Efficiency", round(efficiency, 2), "%")
 
     def emag_calculation(self):
+        """Set emag calculation."""
         self.mcad.show_magnetic_context()
 
-        # Disable all performance tests except the ones for transient torque.
         self.mcad.set_variable("BackEMFCalculation", False)
         self.mcad.set_variable("CoggingTorqueCalculation", False)
         self.mcad.set_variable("ElectromagneticForcesCalc_OC", False)
@@ -90,6 +115,7 @@ class MotorCADSettings:
         self.mcad.do_magnetic_calculation()
 
     def export_settings(self):
+        """Set export settings."""
         self.mcad.set_variable("AnsysExportFormat", 1)
         # 3D export
         self.mcad.set_variable("AnsysModelType", 1)
