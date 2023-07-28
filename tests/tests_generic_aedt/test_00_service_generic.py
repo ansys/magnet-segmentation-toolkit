d853ba142b23e6917670c198674142ead20429c7
Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
2023-07-28 09:29:08 +0200

    Update test_00_service_generic.py

--- tests/tests_generic_aedt/test_00_service_generic.py
+++ tests/tests_generic_aedt/test_00_service_generic.py
@@ -54,7 +54,6 @@ class TestClass(BasisTest, object):
         response = requests.post(self.url + "/save_project", json=file_name)
         assert response.ok
         response = requests.get(self.url + "/get_status")
-        
         while response.json() != "Backend free":
             time.sleep(1)
             response = requests.get(self.url + "/get_status")
