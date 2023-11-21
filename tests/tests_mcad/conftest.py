0cd7b81af0b963a60f9a8206c84aedc0d35988a5
Sebastien Morais <sebastien.morais@ansys.com>
2023-11-21 17:13:38 +0100

    wip motorcad

---
+++ tests/tests_mcad/conftest.py
@@ -0,0 +1,85 @@
+"""
+"""
+import json
+import os
+from pathlib import Path
+import shutil
+import time
+
+from pyaedt import generate_unique_folder_name
+from pyaedt import settings
+import pytest
+
+from ansys.aedt.toolkits.motor.backend.api import Toolkit
+
+PROJECT_NAME = "e9_built"
+DESIGN_NAME = "Motor-CAD e9"
+
+# Initialize default desktop configuration
+default_version = "2023.1"
+config = {
+    "desktop_version": default_version,
+    "non_graphical": True,
+    "use_grpc": True,
+}
+
+# Check for the local config file, override defaults if found
+local_path = os.path.dirname(os.path.realpath(__file__))
+local_config_file = os.path.join(local_path, "local_config.json")
+if os.path.exists(local_config_file):
+    with open(local_config_file) as f:
+        local_config = json.load(f)
+    config.update(local_config)
+
+# Update pyaedt settings
+settings.enable_error_handler = False
+settings.enable_desktop_logs = False
+settings.use_grpc_api = config.get("use_grpc", True)
+settings.non_graphical = config["non_graphical"]
+
+
+@pytest.fixture
+def basic_toolkit():
+    """Initialize the toolkit."""
+    toolkit = Toolkit()
+
+    yield toolkit
+
+    toolkit.close_motorcad()
+
+
+@pytest.fixture
+def toolkit():
+    """Initialize the toolkit with a temporary MOT file.
+    The AEDT file is created at the beginning of each test and removed after each test.
+    """
+    src_folder = os.path.join(Path(__file__).parents[1], "input_data")
+    temp_folder = shutil.copytree(src_folder, os.path.join(generate_unique_folder_name(), "input_data"))
+    mot_file = os.path.join(temp_folder, "e9_built.mot")
+    toolkit = Toolkit()
+    toolkit.set_properties({"MotorCAD_filepath": mot_file})
+    # toolkit.set_properties({"aedt_version": "2023.1"})
+    # toolkit.set_properties({"active_project": aedt_file})
+    # toolkit.set_properties({"non_graphical": config["non_graphical"]})
+    # toolkit.set_properties({"use_grpc": config["use_grpc"]})
+    # src_folder = os.path.join(Path(__file__).parents[1], "input_data")
+    # temp_folder = shutil.copytree(src_folder, os.path.join(generate_unique_folder_name(), "input_data"))
+    # self.toolkit.set_properties({"MotorCAD_filepath": mot_file})
+    # mot_file = os.path.join(temp_folder, "e9_built.mot")
+    yield toolkit
+
+    toolkit.close_motorcad()
+
+
+# src_folder = os.path.join(Path(__file__).parents[1], "input_data")
+#         temp_folder = shutil.copytree(src_folder, os.path.join(generate_unique_folder_name(), "input_data"))
+# self.toolkit.set_properties({"MotorCAD_filepath": mot_file})
+#         mot_file = os.path.join(temp_folder, "e9_built.mot")
+
+
+def wait_toolkit(toolkit):
+    """Wait for the backend to be running."""
+    response = toolkit.get_thread_status()
+    while response[0] == 0:
+        time.sleep(1)
+        response = toolkit.get_thread_status()
--- tests/tests_mcad/test_03_mcad.py
+++ tests/tests_mcad/test_03_mcad.py
@@ -1,116 +1,90 @@
-import gc
 import os
-from pathlib import Path
-import shutil
-import time
 
-from pyaedt import generate_unique_folder_name
 import pytest
 
-from ansys.aedt.toolkits.motor.backend.api import Toolkit
+from tests.tests_mcad.conftest import PROJECT_NAME
+from tests.tests_mcad.conftest import wait_toolkit
 
 pytestmark = [pytest.mark.mcad]
 
 
 class TestClass(object):
-    def setup_class(self):
-        self.toolkit = Toolkit()
-        src_folder = os.path.join(Path(__file__).parents[1], "input_data")
-        self.temp_folder = shutil.copytree(src_folder, os.path.join(generate_unique_folder_name(), "input_data"))
+    def test_init_motorcad(self, basic_toolkit):
+        assert basic_toolkit.init_motorcad()
 
-    def teardown_class(self):
-        if self.toolkit.mcad:
-            self.toolkit.mcad.quit()
-        # Register the cleanup function to be called on script exit
-        gc.collect()
-        shutil.rmtree(self.temp_folder, ignore_errors=True)
+    def test_2_load_default_mcad_file(self, basic_toolkit):
+        assert basic_toolkit.init_motorcad()
+        assert basic_toolkit.load_mcad_file()
 
-    def test_1_init_motorcad(self):
-        assert self.toolkit.init_motorcad()
+        wait_toolkit(basic_toolkit)
 
-    def test_2_load_mcad_file(self):
-        mot_file = os.path.join(self.temp_folder, "e9_built.mot")
-        assert self.toolkit.load_mcad_file()
-        response = self.toolkit.get_thread_status()
-        while response[0] == 0:
-            time.sleep(1)
-            response = self.toolkit.get_thread_status()
-        assert (
-            os.path.splitext(os.path.basename(self.toolkit.mcad.get_variable("CurrentMotFilePath_MotorLAB")))[0]
-            == "default"
-        )
-        self.toolkit.set_properties({"MotorCAD_filepath": mot_file})
-        assert self.toolkit.load_mcad_file()
-        response = self.toolkit.get_thread_status()
-        while response[0] == 0:
-            time.sleep(1)
-            response = self.toolkit.get_thread_status()
-        assert (
-            os.path.splitext(os.path.basename(self.toolkit.mcad.get_variable("CurrentMotFilePath_MotorLAB")))[0]
-            == "e9_built"
-        )
+        value = basic_toolkit.mcad.get_variable("CurrentMotFilePath_MotorLAB")
+        assert os.path.splitext(os.path.basename(value))[0] == "default"
 
-    def test_3_vbs_file_path(self):
-        assert (os.path.splitext(os.path.basename(self.toolkit.mcad.get_variable("CurrentMotFilePath_MotorLAB"))))[
-            0
-        ] + ".vbs" == "e9_built.vbs"
+    def test_2_load_mcad_file(self, toolkit):
+        assert toolkit.init_motorcad()
+        assert toolkit.load_mcad_file()
 
-    def test_4_set_emag_model(self):
-        props = self.toolkit.get_properties()
+        wait_toolkit()
+
+        value = toolkit.mcad.get_variable("CurrentMotFilePath_MotorLAB")
+        assert os.path.splitext(os.path.basename(value))[0] == f"{PROJECT_NAME}"
+
+    def test_4_set_emag_model(self, toolkit):
+        props = toolkit.get_properties()
         props["E_mag_settings"]["NumberOfCuboids"] = 4
-        self.toolkit.set_properties(props)
-        self.toolkit.set_emag_model()
-        assert self.toolkit.mcad.get_variable("NumberOfCuboids") == 4
+        toolkit.set_properties(props)
+        toolkit.set_emag_model()
+        assert toolkit.mcad.get_variable("NumberOfCuboids") == 4
 
-    def test_5_lab_performance_calculation(self):
-        props = self.toolkit.get_properties()
+    def test_5_lab_performance_calculation(self, toolkit):
+        props = toolkit.get_properties()
         props["LAB_settings"]["MaxSpeed"] = 2000
         props["LAB_settings"]["SpeedStep"] = 1000
         props["LAB_settings"]["SpeedMin"] = 0
-        self.toolkit.set_properties(props)
-        self.toolkit.lab_performance_calculation()
-        assert self.toolkit.mcad.get_variable("SpeedMax_MotorLAB") == 2000
-        assert self.toolkit.mcad.get_variable("Speedinc_MotorLAB") == 1000
-        assert self.toolkit.mcad.get_variable("SpeedMin_MotorLAB") == 0
+        toolkit.set_properties(props)
+        toolkit.lab_performance_calculation()
+        assert toolkit.mcad.get_variable("SpeedMax_MotorLAB") == 2000
+        assert toolkit.mcad.get_variable("Speedinc_MotorLAB") == 1000
+        assert toolkit.mcad.get_variable("SpeedMin_MotorLAB") == 0
 
-    def test_6_lab_operating_point(self):
-        props = self.toolkit.get_properties()
+    def test_6_lab_operating_point(self, toolkit):
+        props = toolkit.get_properties()
         props["LAB_settings"]["MaxTempMagnet"] = 150
         props["LAB_settings"]["MaxTempStatorWinding"] = 180
         props["LAB_settings"]["OPSpeed"] = 4500
-        self.toolkit.set_properties(props)
-        self.toolkit.lab_operating_point()
-        assert self.toolkit.mcad.get_variable("MaxMagnet_MotorLAB") == 150
-        assert self.toolkit.mcad.get_variable("StatorTempDemand_Lab") == 180
+        toolkit.set_properties(props)
+        toolkit.lab_operating_point()
+        assert toolkit.mcad.get_variable("MaxMagnet_MotorLAB") == 150
+        assert toolkit.mcad.get_variable("StatorTempDemand_Lab") == 180
 
-    def test_7_emag_calculation(self):
-        self.toolkit.emag_calculation()
-        assert not self.toolkit.mcad.get_variable("BackEMFCalculation")
-        assert not self.toolkit.mcad.get_variable("CoggingTorqueCalculation")
-        assert self.toolkit.mcad.get_variable("TorqueCalculation")
+    def test_7_emag_calculation(self, toolkit):
+        toolkit.emag_calculation()
+        assert not toolkit.mcad.get_variable("BackEMFCalculation")
+        assert not toolkit.mcad.get_variable("CoggingTorqueCalculation")
+        assert toolkit.mcad.get_variable("TorqueCalculation")
 
-    def test_8_set_thermal(self):
+    def test_8_set_thermal(self, toolkit):
         magnet_losses = {"SolidLoss": {"Value": 1.5}}
-        self.toolkit.set_thermal(magnet_losses)
-        assert self.toolkit.mcad.get_variable("ThermalCalcType") == 0
-        assert self.toolkit.mcad.get_variable("Magnet_Iron_Loss_@Ref_Speed") == 1.5
+        toolkit.set_thermal(magnet_losses)
+        assert toolkit.mcad.get_variable("ThermalCalcType") == 0
+        assert toolkit.mcad.get_variable("Magnet_Iron_Loss_@Ref_Speed") == 1.5
 
-    def test_9_thermal_calculation(self):
-        assert isinstance(self.toolkit.thermal_calculation(), float)
+    def test_9_thermal_calculation(self, toolkit):
+        assert isinstance(toolkit.thermal_calculation(), float)
 
-    def test_10_export(self):
-        self.toolkit.export_settings()
-        response = self.toolkit.get_thread_status()
-        while response[0] == 0:
-            time.sleep(1)
-            response = self.toolkit.get_thread_status()
-        vbs_file_path = self.toolkit.get_properties()["vbs_file_path"]
+    def test_10_export(self, toolkit):
+        toolkit.export_settings()
+
+        wait_toolkit(toolkit)
+
+        vbs_file_path = toolkit.get_properties()["vbs_file_path"]
         os.path.exists(vbs_file_path)
-        assert self.toolkit.mcad.get_variable("AxialSegments") == 1
+        assert toolkit.mcad.get_variable("AxialSegments") == 1
 
-    def test_11_save(self):
-        assert self.toolkit.save()
+    def test_11_save(self, toolkit):
+        assert toolkit.save()
 
-    def test_12_close(self):
-        assert self.toolkit.close_motorcad()
-        self.toolkit.mcad = None
+    def test_12_close(self, toolkit):
+        assert toolkit.close_motorcad()
+        toolkit.mcad = None
