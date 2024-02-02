87fb7794ccb7cd199209cd58c7a779ab0feb59bd
Sebastien Morais <sebastien.morais@ansys.com>
2024-02-02 23:59:11 +0100

    DOC: rework examples

--- examples/index.rst
+++ examples/index.rst
@@ -1,5 +1,7 @@
-.. _ref_example_gallery:
-
 Examples
 ========
 End-to-end examples show how you can use the Magnet Segmentation Toolkit.
+
+.. nbgallery::
+
+    maxwell3d_segmentation_skew.py
--- examples/maxwell3d_segmentation_skew.py
+++ examples/maxwell3d_segmentation_skew.py
@@ -1,52 +1,53 @@
-"""
-Maxwell 3D segmentation
------------------------
-This example shows how to use the Magnet Segmentation Toolkit to segment
-your AEDT motor model.
-"""
+# # Maxwell 3D segmentation
+#
+# This example shows how to use the Magnet Segmentation Toolkit to segment
+# your AEDT motor model.
 
-#################################################################################
-# Perform required imports
-# ~~~~~~~~~~~~~~~~~~~~~~~~
+# ## Perform required imports
+#
 # Perform required imports.
 
+# +
 import os
-from pathlib import Path
 import shutil
 
 from pyaedt import generate_unique_folder_name
 
 from ansys.aedt.toolkits.magnet_segmentation.backend.api import Toolkit
+from ansys.aedt.toolkits.magnet_segmentation.utils import download_file
 
-#################################################################################
-# Initialize temporary folder and project settings
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# -
+
+# ## Initialize temporary folder and project settings
+#
 # Initialize a temporary folder to copy the input file into
 # and specify project settings.
 
-src_folder = os.path.join(Path(__file__).parents[0], "input_files")
-temp_folder = shutil.copytree(src_folder, os.path.join(generate_unique_folder_name(), "input_files"))
-project_name = "e9_eMobility_IPM_3D"
-active_project = os.path.join(temp_folder, "{}.aedt".format(project_name))
-active_design = "e9_eMobility_IPM_3D_test"
+# +
+URL_BASE = "https://raw.githubusercontent.com/ansys/example-data/master/toolkits/magnet_segmentation/"
+AEDT_PROJECT = "e9_eMobility_IPM_3D.aedt"
+URL = os.path.join(URL_BASE, AEDT_PROJECT)
 
-#################################################################################
-# Initialize toolkit
-# ~~~~~~~~~~~~~~~~~~
+temp_folder = os.path.join(generate_unique_folder_name())
+active_project = os.path.join(temp_folder, AEDT_PROJECT)
+download_file(URL, active_project)
+active_design = "e9_eMobility_IPM_3D_test"
+# -
+
+# ## Initialize toolkit
+#
 # Initialize the toolkit.
 
 toolkit = Toolkit()
 
-#################################################################################
-# Get toolkit properties
-# ~~~~~~~~~~~~~~~~~~~~~~
+# ## Get toolkit properties
+#
 # Get the toolkit properties.
 
 properties = toolkit.get_properties()
 
-#################################################################################
-# Initialize properties
-# ~~~~~~~~~~~~~~~~~~~~~
+# ## Initialize properties
+#
 # Initialize a dictionary of properties.
 
 properties["active_project"] = active_project
@@ -62,60 +63,52 @@ properties["rotor_slices"] = 2
 properties["apply_mesh_sheets"] = True
 properties["mesh_sheets_number"] = 3
 
-##########################################################
-# Set non-graphical mode
-# ~~~~~~~~~~~~~~~~~~~~~~
+# ## Set non-graphical mode
+#
 # Set non-graphical mode. The default value is ``False``.
 
 properties["non_graphical"] = False
 
-#################################################################################
-# Set properties
-# ~~~~~~~~~~~~~~
+# ## Set properties
+#
 # Set properties.
 
 toolkit.set_properties(properties)
 
-#################################################################################
-# Initialize AEDT
-# ~~~~~~~~~~~~~~~
+# ## Initialize AEDT
+#
 # Initialize AEDT.
 
 toolkit.launch_aedt()
 
-#################################################################################
-# Wait for the toolkit thread to be idle
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ## Wait for the toolkit thread to be idle
+#
 # Wait for the toolkit thread to be idle and ready to accept a new task.
 
 toolkit.wait_to_be_idle()
 
-#################################################################################
-# Apply segmentation
-# ~~~~~~~~~~~~~~~~~~
+# ## Apply segmentation
+#
 # Apply segmentation and assign the relative coordinate system.
 
 toolkit.segmentation()
 
-#################################################################################
-# Apply skew angle
-# ~~~~~~~~~~~~~~~~
+# ## Apply skew angle
+#
 # Apply the skew angle to rotor slices.
 
 toolkit.apply_skew()
 
-#################################################################################
-# Save and release AEDT
-# ~~~~~~~~~~~~~~~~~~~~~
+# ## Save and release AEDT
+#
 # Save and release AEDT.
 
 # toolkit.save_project()
 
 toolkit.release_aedt(True, True)
 
-#################################################################################
-# Remove temporary folder
-# ~~~~~~~~~~~~~~~~~~~~~~~
+# ## Remove temporary folder
+#
 # Remove the temporary folder.
 
 shutil.rmtree(temp_folder, ignore_errors=True)
