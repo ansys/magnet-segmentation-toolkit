287e955b6168ae2d99b740e15dd60b78fef0b248
Giulia Malinverno <giulia.malinverno@ansys.com>
2024-02-29 16:49:31 +0100

    comment lines in example

--- examples/maxwell3d_segmentation_skew.py
+++ examples/maxwell3d_segmentation_skew.py
@@ -104,14 +104,14 @@ toolkit.apply_skew()
 #
 # Uncomment the line to validate and analyze the design.
 
-#toolkit.validate_and_analyze()
+# toolkit.validate_and_analyze()
 
 # ## Create magnet loss report
 #
 # Uncomment the lines to create magnet loss report and compute average value.
 
-#magnet_loss = toolkit.get_magnet_loss()
-#print(f"Average magnet loss: {magnet_loss['SolidLoss']['Value']}{magnet_loss['SolidLoss']['Unit']}")
+# magnet_loss = toolkit.get_magnet_loss()
+# print(f"Average magnet loss: {magnet_loss['SolidLoss']['Value']}{magnet_loss['SolidLoss']['Unit']}")
 
 # ## Save and release AEDT
 #
