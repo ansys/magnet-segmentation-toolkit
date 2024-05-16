b621157ec06d46ec161b62abf7d6f6e3f1f288aa
Sébastien Morais <146729917+SMoraisAnsys@users.noreply.github.com>
2024-05-16 02:52:12 -0400

    DOCS: Fix build html (#217)
    * DOCS: Fix typo in class name
    
    * DOCS: Fix build-finished sphinx hook
    
    * CHORE: Fix precommit

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -70,7 +70,7 @@ def check_example_error(app, pagename, context):
             app.builder.config.html_context["build_error"] = True
 
 
-def check_build_finished_without_error(app):
+def check_build_finished_without_error(app, exception):
     """Check that no error is detected along the documentation build process."""
     if app.builder.config.html_context.get("build_error", False):
         raise Exception("Build failed due to an error in html-page-context")
